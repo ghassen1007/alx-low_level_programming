0e69fc3121587ca63dd10ea324335457e6a89b3f
ghassen1007 <mabroukighassen07@gmail.com>
2023-09-07 16:06:24 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void)
+/**
+ * main entry
+ * descreption programm that prints with printf function
+ * return 0
+ */
+	
+{
+printf ("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
+
+
+
+
