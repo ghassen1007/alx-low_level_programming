59ac66d802a3b52a377a261bd4c04dc4160976ac
ghassen1007 <mabroukighassen07@gmail.com>
2023-09-11 18:00:54 -0700

    2

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point 
+ * descreption : print all alhabet letters
+ * return always 0 success*/
+int main(void)
+{
+  char ch = 'a';
+          while ( ch <= 'z')
+            { putchar(ch);
+            ch++ ;
+}
+putchar('\n');
+return (0);
+}
